f2fc50890c981caaf37951b49e6c4c917defa9c7
marc-antoine <marcantoine.bruelhart@gmail.com>
2020-03-03 12:06:52 +0100

    replace module visits to student controller

--- src/main/java/ch/mab/plansch/demo/controller/ModuleController.java
+++ src/main/java/ch/mab/plansch/demo/controller/ModuleController.java
@@ -1,7 +1,9 @@
 package ch.mab.plansch.demo.controller;
 
-import ch.mab.plansch.demo.model.ModuleVisit;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,22 +16,5 @@ public class ModuleController {
     @GetMapping("{studentid}")
     public List<Module> retrieveAllModules(@PathVariable("studentid") UUID studentId) {
         return Collections.emptyList();
-    }
-
-    @GetMapping("visits/{studentid}")
-    public List<ModuleVisit> retrieveVisitsModules(@PathVariable("studentid") UUID studentId) {
-        return Collections.emptyList();
-    }
-
-    @PostMapping("visits/{studentid}")
-    public void createModuleVisit(@PathVariable("studentid") UUID studentId, @RequestBody ModuleVisit moduleVisit) {
-    }
-
-    @PutMapping("visits/{studentid}/{id}")
-    public void updateModuleVisit(@PathVariable("studentid") UUID studentId, @PathVariable("id") UUID moduleVisitId, @RequestBody ModuleVisit moduleVisit) {
-    }
-
-    @DeleteMapping("visits/{studentid}/{id}")
-    public void deleteModulVisit(@PathVariable("studentid") UUID studentId, @PathVariable("id") UUID moduleVisitId) {
     }
 }
--- src/main/java/ch/mab/plansch/demo/controller/StudentController.java
+++ src/main/java/ch/mab/plansch/demo/controller/StudentController.java
@@ -1,10 +1,13 @@
 package ch.mab.plansch.demo.controller;
 
+import ch.mab.plansch.demo.model.ModuleVisit;
 import ch.mab.plansch.demo.model.Student;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -13,8 +16,8 @@ import java.util.UUID;
 public class StudentController {
 
     @ApiOperation(value = "Retrieve student details")
-    @GetMapping("{id}")
-    public Student retrieveStudent(@PathVariable("id") UUID id) {
+    @GetMapping("{studentid}")
+    public Student retrieveStudent(@PathVariable("studentid") UUID id) {
         return new Student();
     }
 
@@ -22,5 +25,27 @@ public class StudentController {
     @PostMapping
     public Student createStudent(@RequestBody Student student) {
         return new Student();
+    }
+
+    @ApiOperation(value = "Retrieve module visits by student")
+    @GetMapping("{studentid}/modulevisits")
+    public Set<ModuleVisit> retrieveModuleVisitsByStudentId(@PathVariable("studentid") UUID studentId) {
+        return Collections.emptySet();
+    }
+
+    @PostMapping("{studentid}/modulevisits")
+    public void createModuleVisitByStudentId(@PathVariable("studentid") UUID studentId,
+                                             @RequestBody ModuleVisit moduleVisit) {
+    }
+
+    @PutMapping("{studentid}/modulevisits/{id}")
+    public void updateModuleVisitByStudentIdAndModuleVisitId(@PathVariable("studentid") UUID studentId,
+                                                             @PathVariable("id") UUID moduleVisitId,
+                                                             @RequestBody ModuleVisit moduleVisit) {
+    }
+
+    @DeleteMapping("{studentid}/modulevisits/{id}")
+    public void deleteModulVisitByStudentIdAndModuleVisitId(@PathVariable("studentid") UUID studentId,
+                                                            @PathVariable("id") UUID moduleVisitId) {
     }
 }
