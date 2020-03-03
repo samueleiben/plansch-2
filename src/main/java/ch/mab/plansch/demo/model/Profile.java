7a0298f166ebf33d0842f65204ac6045e64dc06e
Marc-Antoine <16390193+mab9@users.noreply.github.com>
2020-03-03 21:15:50 +0100

    Add comment

--- src/main/java/ch/mab/plansch/demo/model/Profile.java
+++ src/main/java/ch/mab/plansch/demo/model/Profile.java
@@ -7,6 +7,9 @@ import java.util.UUID;
  * Model für die Profilierung
  * Name kann noch angepasst werden
  */
+
+// Warum braucht es das Profile? Kann ja direkt über die ModuleGroup abgehandlet werden. Denn die macht ja eigentlich genau 
+// das gleiche.
 public class Profile {
     UUID id;
     String name;
