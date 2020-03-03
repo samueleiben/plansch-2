485da075978948768e9c2aecd3aa8db00e7db219
rogerkreienbuehl <r.kreienbuehl>
2020-03-03 12:33:22 +0100

    changed PathParam to PathVariable

--- src/main/java/ch/mab/plansch/demo/controller/ModuleController.java
+++ src/main/java/ch/mab/plansch/demo/controller/ModuleController.java
@@ -4,7 +4,6 @@ import ch.mab.plansch.demo.model.ModuleVisit;
 import ch.mab.plansch.demo.model.Module;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -23,7 +22,7 @@ public class ModuleController {
     }
 
     @GetMapping(path="/{id}")
-    public Module retrieveModule(@PathParam("id") String id) {
+    public Module retrieveModule(@PathVariable("id") String id) {
         return new Module();
     }
 
