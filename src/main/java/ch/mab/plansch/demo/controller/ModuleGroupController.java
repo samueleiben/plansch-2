546fb814df88d8af0c3f22e27b56abfc2f63a439
marc-antoine <marcantoine.bruelhart@gmail.com>
2020-03-03 12:10:31 +0100

    Grammer erros.

--- src/main/java/ch/mab/plansch/demo/controller/ModuleGroupController.java
+++ src/main/java/ch/mab/plansch/demo/controller/ModuleGroupController.java
@@ -1,6 +1,6 @@
 package ch.mab.plansch.demo.controller;
 
-import ch.mab.plansch.demo.model.ModulGroup;
+import ch.mab.plansch.demo.model.ModuleGroup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +12,15 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("modulegroups")
-public class ModulGroupController {
+public class ModuleGroupController {
 
     @GetMapping("{studentid}")
-    public List<ModulGroup> retrieveAllModuleGroups(@PathVariable("studentid") UUID studentId) {
+    public List<ModuleGroup> retrieveAllModuleGroups(@PathVariable("studentid") UUID studentId) {
         return Collections.emptyList();
     }
 
     @GetMapping("{studentid}/{id}")
-    public List<ModulGroup> retrieveModuleGroupById(@PathVariable("studentid") UUID studentId) {
+    public List<ModuleGroup> retrieveModuleGroupById(@PathVariable("studentid") UUID studentId) {
         return Collections.emptyList();
     }
 }
