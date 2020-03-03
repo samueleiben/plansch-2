ddb68622ed29e86c35a22a1a56288b09b7704d59
Dominik Steinmann <dominik.steinmann@students.fhnw.ch>
2020-03-03 15:17:05 +0100

    vorschlag für module model anhang graphql schnittstelle

--- src/main/java/ch/mab/plansch/demo/model/Module.java
+++ src/main/java/ch/mab/plansch/demo/model/Module.java
@@ -4,13 +4,16 @@ import java.util.Set;
 import java.util.UUID;
 
 public class Module {
-
+    // Vorschlag für Module anhand der GraphQL Schnittstelle von Gruntz
     UUID id; // Macht keinen Sinn, Modul-ID ist bereits vorhanden in den Daten der Module
     String name;
-    String description;
+    String code;
     int credits;
+    boolean hs;
+    boolean fs;
     // boolean isMandatory; // wird nicht benötigt, Diskussion von gestern (02.03.2020)
-    String[] language; // muss ja kein String sein... rapid prototype
-    Set<UUID> prerequesites;
+    //String[] language; // muss ja kein String sein... rapid prototype
+    ModuleGroup group;
+    Set<UUID> requirements;
 
 }
